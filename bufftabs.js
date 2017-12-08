1491a4448c2a60ee43792ddbefe9e7078592ecaf
Nathan Eikermann <tensazangetsu1990@googlemail.com>
2017-12-08 22:33:37 +0100

    added feature to toggle and switch to bufferlist tab

--- bufftabs.js
+++ bufftabs.js
@@ -5,7 +5,57 @@ function openBuffList() {
     });
 }
 
+function getCurrentWindowTabs() {
+    return browser.tabs.query({currentWindow: true});
+}
+
+
+
+function logTabs(tabs) {
+    console.log(tabs);
+    for (let tab of tabs) {
+        console.log(tab.url);
+    }
+}
+
+function onError(error) {
+    console.log("Error: ", error);
+}
+
+function fillTabList() {
+    getCurrentWindowTabs().then((tabs) => {
+        
+    });    
+}
+
 browser.commands.onCommand.addListener((command) => {
     console.log("Received Command: ", command);
-    openBuffList();
+    var querying = browser.tabs.query({url: browser.extension.getURL("*")});
+    querying.then(logTabs, onError);
+    querying.then((tabs) => {
+        if (tabs.length == 0) {
+            openBuffList();
+        
+        } else {
+            var active = false;
+            var tabid;
+
+            for (let tab of tabs) {
+                if(tab.active) {
+                    console.log("Removing tab with tablist");
+                    browser.tabs.remove(tab.id);
+                    active = true;
+                    break;
+                }
+                tabid = tab.id;
+            }
+
+            if (!active) {
+                console.log("Switching to tab with tablist");
+                browser.tabs.update(tabid, {
+                    active: true
+                });
+            }
+        }
+    });
 });
